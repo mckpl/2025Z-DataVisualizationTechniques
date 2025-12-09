749357fc755394d1df136522c7fdfe4a711d9bda
Łukasz Krawczyk <lukasz.krawczyk04@gmail.com>
2025-12-09 23:47:43 +0100

    [HW4]

---
+++ homeworks/hw4/KrawczykLukasz/wykres_generator.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+data <- readr::read_csv(
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-03-26/public-picks.csv"
+)
+
+data_long <- pivot_longer(
+  data,
+  -c(YEAR, TEAMNO, TEAM),
+  names_to = "Round",
+  values_to = "Percentage"
+)
+
+data_long$Percentage <- as.numeric(sub("%", "", data_long$Percentage))
+
+p <- ggplot(data_long, aes(x = Round, y = Percentage, fill = TEAM)) +
+  geom_col(position = "dodge") +
+  facet_wrap(~YEAR) +
+  labs(
+    title = "Progression of NCAA basketball teams across different rounds",
+    subtitle = "Source: 2024 Tidy Tuesday week 13",
+    x = "Round",
+    y = "Percentage"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "#F5F5DC", color = NA),   # beżowe tło
+    panel.background = element_rect(fill = "#F5F5DC", color = NA),
+    legend.background = element_rect(fill = "#F5F5DC"),
+    text = element_text(family = "Arial", color = "black"),
+    plot.title = element_text(size = 18, face = "bold"),
+    legend.position = "bottom"
+  )
+
+ggsave("Poprawiony_Wykres.png", p, width = 10, height = 10)
